549da9380bc4892ad936a01a7f9e25c3e00952c9
Mufaddal Motorwala <mufaddal.motorwala@gmail.com>
2017-12-02 16:27:09 +0400

    Contact, tabs section done

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -6,6 +6,7 @@ import foundation from 'foundation-sites';
 // init foundation
 $(document).foundation();
 
+import { header } from "../../pages/components/header/header";
 import { homebanner } from "../../pages/components/banner/banner";
 import { homecarousel } from "../../pages/components/carousel/carousel";
 
---
+++ src/pages/components/header/header.js
@@ -0,0 +1,12 @@
+let headerscroll = function(){
+    
+    if ( $(this).scrollTop() > 100 ) {
+        $('.js-header').addClass('is-sticky');
+    }
+    else {
+        $('.js-header').removeClass('is-sticky');
+    }
+    
+}
+
+$(window).scroll(headerscroll);
