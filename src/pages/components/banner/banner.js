e03cbfb016c3b7a29bb4d4a7711d6460e80f211b
Mufaddal Motorwala <mufaddal.motorwala@gmail.com>
2017-12-04 19:25:24 +0400

    Added window.load for banner Js

--- src/pages/components/banner/banner.js
+++ src/pages/components/banner/banner.js
@@ -4,6 +4,10 @@ import Swiper from "swiper";
 const banner = {
     init: function(){
         this.homebanner();
+        this.handleEvents();
+    },
+    handleEvents: function(){
+        $(window).load(this.handleEvents);
     },
     homebanner: function() {
         let swiper = new Swiper('.js-home-banner', {
